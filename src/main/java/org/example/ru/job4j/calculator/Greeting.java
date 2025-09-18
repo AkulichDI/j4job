4e3a301bec1df7e8187a2706a8e22c9a23ad0f07
Gerik228 <81228315+Gerik228@users.noreply.github.com>
2025-09-18 14:02:12 +1000

    18.09.2025 14:01

---
+++ src/main/java/org/example/ru/job4j/calculator/Greeting.java
@@ -0,0 +1,14 @@
+package org.example.ru.job4j.calculator;
+
+public class Greeting {
+    public static void main ( String[] args ) {
+        String idea = "I like Java!";
+        idea += "But I am a newbie.";
+        int year = 2025;
+        idea += year;
+        System.out.println(idea);
+    }
+
+
+
+}
