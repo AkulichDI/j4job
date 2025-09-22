624554bfe2071fc39d426f0da3055b16f24abe6e
Gerik228 <dimaakulichd6618@gmail.com>
2025-09-22 15:57:22 +1000

    new

--- src/test/java/testirovanie/VesTest.java
+++ src/test/java/testirovanie/VesTest.java
@@ -1,8 +1,6 @@
 package testirovanie;
 
-import OperatorVetvlenia.DummyBot;
-import OperatorVetvlenia.LogicNot;
-import OperatorVetvlenia.Max;
+import OperatorVetvlenia.*;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -298,4 +296,185 @@ class  LogicNotTest{
         assertThat(result).isFalse();
     }
 
+}
+
+class TriangleTest {
+        @Test
+        void whenExistTrue() {
+            double ab = 2.0;
+            double ac = 2.0;
+            double bc = 2.0;
+            boolean result = Triangle.exist(ab, ac, bc);
+            assertThat(result).isTrue();
+        }
+        @Test
+        void  whenNotExist() {
+            double ab = -800;
+            double ac = 90;
+            double bc = 90;
+            boolean result = Triangle.exist(ab, ac, bc);
+            assertThat(result).isFalse();
+        }
+
+
+
+}
+
+
+
+class ChessBoardTest {
+    @Test
+    public void wayIs5() {
+        int x1 = 6;
+        int y1 = 7;
+        int x2 = 1;
+        int y2 = 2;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void wayIs7() {
+        int x1 = 7;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 7;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void wayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenX1isMinus1ThenWayIs0() {
+        int x1 = -1;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenY1isMinus1ThenWayIs0() {
+        int x1 = 2;
+        int y1 = -1;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenX2isMinus1ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = -1;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenY2isMinus1ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = -1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenX1isGreater7ThenWayIs0() {
+        int x1 = 10;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenY1isGreater7ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 10;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenX2isGreater7ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 10;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenY2isGreater7ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 10;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+}
+
+class DivideBySixTest {
+    @Test
+    void whenNumberDivideBy6() {
+        int input = 24;
+        String result = DivideBySix.checkNumber(input);
+        String expected = "The number divides by 6.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenNumberDivideBy3AndNotEven() {
+        int input = 9;
+        String result = DivideBySix.checkNumber(input);
+        String expected = "The number divides by 3, but it isn't the even number.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenNumberNoDivideBy3AndEven() {
+        int input = 14;
+        String result = DivideBySix.checkNumber(input);
+        String expected = "The number doesn't divide by 3, but it is the even number.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenNumberNoDivideBy3AndNotEven() {
+        int input = 25;
+        String result = DivideBySix.checkNumber(input);
+        String expected = "The number doesn't divide by 3 and it isn't the even number.";
+        assertThat(result).isEqualTo(expected);
+    }
 }
