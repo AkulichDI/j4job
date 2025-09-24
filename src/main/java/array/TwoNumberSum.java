cd4b6bce77f5f34da3d21eda298e87628aba3966
Gerik228 <dimaakulichd6618@gmail.com>
2025-09-24 13:21:30 +1000

    Закончил с массивами

--- src/main/java/array/TwoNumberSum.java
+++ src/main/java/array/TwoNumberSum.java
@@ -1,23 +1,22 @@
 package array;
-
+//6.8.0
 public class TwoNumberSum {
 
     public static int[] getIndexes(int[] array, int target) {
         int i = 0;
-        int j = i + 1 ;
-        while (i < j){
+        int j = 1 ;
+        while (i < array.length){
             if (array[i] + array[j] == target){
-
                 return new int[] {i, j};
 
+            }if (array[i] + array[j] != target) {
+                j++;
 
+            }if (j == array.length){
+                j = i +1;
+                i++;
             }
-            j++;
-            i++;
         }
-
-
-
         return new int[0];
     }
 
