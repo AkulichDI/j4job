27d5ef2d99f02cc47b1e0b7b94c9f424c222e592
Gerik228 <81228315+Gerik228@users.noreply.github.com>
2025-10-03 11:01:36 +1000

    3. Массивы и модели (Инкапсуляция) 1. Создайте модель данных "Книга" с полями: имя и количество страниц. ru.job4j.pojo.Book.
    2. Создайте класс ru.job4j.pojo.Library и в нем метод main.
    
    3. В методе main создайте 4 объекта Book. Добавьте их в массив. Сделайте одну книгу с именем "Clean code".
    
    4. Выведите содержимое массива на консоль через цикл for + index.
    
    5. Переставьте местами книги с индексом 0 и 3. Выведите содержимое массива на консоль.
    
    6. Добавьте цикл с выводом книг с именем "Clean code".

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,47 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book test1 = new Book("Clean code", 120);
+        Book test2 = new Book("java mdl", 190);
+        Book test3 = new Book("java sn", 1000);
+        Book test4 = new Book("java GOD", 10000);
+        Book [] books = new Book[4];
+        books[0] = test1;
+        books[1] = test2;
+        books[2] = test3;
+        books[3] = test4;
+
+        for (int i = 0; i < books.length; i++) {
+
+            Book book = books[i];
+            System.out.println("Наименование книги: "  +  book.getName()+ "\t  Количество страниц: " + book.getCount());
+
+        }
+
+        System.out.println("Перестановка 0 и 3 ");
+
+        Book tmp = books[3];
+        books[3] = books[0];
+        books[0] = tmp;
+
+        for (int i = 0; i < books.length; i++) {
+
+            Book book = books[i];
+            System.out.println("Наименование книги: "  +  book.getName()+ "\t  Количество страниц: " + book.getCount());
+
+        }
+
+        System.out.println("Поиск книги по названию");
+        for (int i = 0; i < books.length; i++) {
+
+            Book book = books[i];
+            if (book.getName().equals("Clean code")){
+                System.out.println("Наименование книги: "  +  book.getName()+ "\t  Количество страниц: " + book.getCount());
+            }
+        }
+
+    }
+
+}
