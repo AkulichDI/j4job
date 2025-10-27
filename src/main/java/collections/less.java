c70f7ab3dc38fd9efe42f411e799bfdea585c41f
Gerik228 <dimaakulichd6618@gmail.com>
2025-10-27 16:27:37 +1000

    5. Очередь с приоритетом на LinkedList

---
+++ src/main/java/collections/less.java
@@ -0,0 +1,12 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class less {
+    public static void main(String[] args) {
+        List<String> tst = new ArrayList<>();
+
+
+    }
+}
---
+++ src/main/java/collectionsLinkedList/Task.java
@@ -0,0 +1,20 @@
+package collectionsLinkedList;
+
+public class Task {
+    private String description;
+    private int priority;
+
+    public Task (String description, int priority){
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
