fd3ccc5d9afe815e38ce937de2ad7304fa20b3f9
Gerik228 <dimaakulichd6618@gmail.com>
2025-10-29 17:04:59 +1000

    3. Модели данных и HashSet. нач

---
+++ src/main/java/collections/NotifyAccount.java
@@ -0,0 +1,10 @@
+package collections;
+
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+        8
+        HashSet<Account> result = new HashSet<>();
+        /* for-each accounts to HashSet; */
+        return result;
+    }
+}
