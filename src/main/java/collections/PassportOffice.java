92241f25ee3da56d445bdcf86a0c6d8d351e369e
Gerik228 <81228315+Gerik228@users.noreply.github.com>
2025-11-06 19:20:58 +1000

    Паспорт и жители

---
+++ src/main/java/collections/PassportOffice.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.*;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean result = false;
+        if (!citizens.containsKey(citizen.getPassport()) ) {
+             citizens.put(citizen.getPassport(), citizen);
+           result = true;
+        }
+        return result;
+    }
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}
---
+++ src/test/java/collections/PassportOfficeTest.java
@@ -0,0 +1,28 @@
+package collections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenTestAddMethodIsFalse() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        boolean firstAdd = office.add(citizen);
+        boolean secondAdd = office.add(citizen);
+        assertThat(firstAdd).isTrue();
+        assertThat(secondAdd).isFalse();
+    }
+
+
+}
