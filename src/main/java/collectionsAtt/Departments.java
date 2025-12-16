b673b426c3a31f5c754c725f48126a8b0234856d
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-16 16:12:48 +1000

    Отсортировать департаменты (Потом)

---
+++ src/main/java/collectionsAtt/Departments.java
@@ -0,0 +1,40 @@
+package collectionsAtt;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Departments {
+    /**
+     *
+     * @param departments
+     * @return
+     */
+    public static List<String> fillGaps(List<String> departments){
+        Set<String> temp = new LinkedHashSet<>();
+        for (String value  : departments){
+            String start = "";
+            for (String element : value.split("/")){
+
+            }
+        }
+
+
+
+
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+
+    public static void sortAsc(List<String> departments){
+
+    }
+
+    public static void sortDesc(List<String> departments){
+
+    }
+
+
+
+}
