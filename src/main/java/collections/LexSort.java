a8d6e8ff133ea795f9f97740fed1214ed09bdd64
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-16 14:05:44 +1000

    Сортировка номера

---
+++ src/main/java/collections/LexSort.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int leftNum = Integer.parseInt(left.split("\\.")[0]);
+        int rightNum = Integer.parseInt(right.split("\\.")[0]);
+        return Integer.compare(leftNum, rightNum);
+    }
+
+
+}
