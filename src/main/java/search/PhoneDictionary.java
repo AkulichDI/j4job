e6be05a8e323dc6f6e9490e4a7d867234e666175
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-17 17:20:58 +1000

    Функции высшего порядка

---
+++ src/main/java/search/PhoneDictionary.java
@@ -0,0 +1,19 @@
+package search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add (Person person){
+        this.persons.add(person);
+    }
+    public ArrayList<Person> find (String key){
+        ArrayList<Person> result = new ArrayList<>();
+        //***
+        return result;
+    }
+
+
+}
