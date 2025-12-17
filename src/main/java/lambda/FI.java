26a5404e309eda1ffe8cc29474509c8afdd4021d
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-17 10:36:14 +1000

    Лямбда (сделал)

--- src/main/java/lambda/FI.java
+++ src/main/java/lambda/FI.java
@@ -1,5 +1,6 @@
 package lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class FI {
@@ -10,12 +11,18 @@ public class FI {
                 new Attachment("image 3" ,120),
                 new Attachment("image 2" ,23)
         };
-        Comparator<Attachment> comparator = new Comparator<Attachment>() {
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return 0;
-            }
-        };
+
+        Comparator<Attachment> comporatorText = (left , right ) -> left.getName().compareTo(right.getName());
+        Comparator<Attachment> comporatorDesc = (left, right) -> Integer.compare(right.getSize(), left.getSize());
+       // Arrays.sort(attachments, comporatorText);
+        Arrays.sort(attachments, comporatorDesc);
+
+
+        for(Attachment obj : attachments){
+            System.out.println("Имя: " + obj.getName()  + "\nSize: " + obj.getSize());
+        }
+
+
     }
 
 
