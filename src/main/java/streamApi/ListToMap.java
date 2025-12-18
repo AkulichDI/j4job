4e95fd16906527f79fdf443532f33cb3f98149d7
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-18 17:24:23 +1000

    Преобразование List в Map (нач)

---
+++ src/main/java/streamApi/ListToMap.java
@@ -0,0 +1,17 @@
+package streamApi;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListToMap {
+
+    public static Map<String, Student> convert(List<Student> list){
+        return new LinkedHashMap<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
