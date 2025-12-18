4ea4b6c780b42ddbfa69fa163b14f02f6776ab65
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-18 11:37:09 +1000

    Лямбда выражения и Collection Framework

---
+++ src/main/java/lambda/IterableLambdaUsage.java
@@ -0,0 +1,18 @@
+package lambda;
+
+import java.util.Arrays;
+
+public class IterableLambdaUsage {
+
+    public static void main(String[] args) {
+        Iterable<String> iterable = Arrays.asList(
+                "name",
+                "top",
+                "user",
+                "precision",
+                "post"
+        );
+        iterable.forEach(System.out::println);
+    }
+
+}
