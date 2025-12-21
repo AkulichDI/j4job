24c99c398bec532c2d085a40f620b4c868be6506
Gerik228 <dimaakulichd6618@gmail.com>
2025-12-22 09:41:23 +1000

    Методы проверки элементов потока на соответствие условию

---
+++ src/main/java/streamApi/MatchExample.java
@@ -0,0 +1,26 @@
+package streamApi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MatchExample {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("Один", "Два", "Три", "Четыре", "Пять");
+        boolean result = strings.stream()
+                .noneMatch("Шесть"::contains);
+        System.out.println(result);
+
+        boolean result1 = strings.stream()
+                .anyMatch(element -> element.endsWith("ь"));
+        // .anyMatch("Один"::contains);
+        System.out.println(result1);
+
+        List<String> strings1 = Arrays.asList("Трижды", "Три", "Триста", "Три тысячи");
+
+        boolean result2 = strings1.stream()
+                .allMatch(element -> element.startsWith("Три"));
+        System.out.println(result2);
+
+    }
+}
