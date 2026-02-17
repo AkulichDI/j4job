f84d06925fd78e408525cc8b121bca21345100f6
Gerik228 <dimaakulichd6618@gmail.com>
2026-02-17 14:41:00 +1000

    MapLambdaUsageLS 5

--- src/main/java/lambda/MapLambdaUsageLS.java
+++ src/main/java/lambda/MapLambdaUsageLS.java
@@ -50,7 +50,7 @@ public class MapLambdaUsageLS {
     }*/
 
 
-
+/*
     public static void main(String[] args) {
 
         Map<String, Integer> map = new HashMap<>();
@@ -58,6 +58,19 @@ public class MapLambdaUsageLS {
         BiFunction<Integer, Integer, Integer> function = (oldValue, newValue) -> oldValue - newValue;
         int newPrice = map.merge("Shoes", 50, function);
         System.out.println("New price: " + newPrice);
+        map.forEach((key, value) -> System.out.println("Key: " + key + ", value: " + value));
+    }*/
+
+
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Shoes", 200);
+        BiFunction<Integer, Integer, Integer> function = (oldValue, newValue) -> oldValue - newValue;
+        int newPrice = map.merge("Shoes", 50, function);
+        System.out.println("New price: " + newPrice);
+        System.out.println("Price of shirt: " + map.merge("Shirt", 100, function));
+
         map.forEach((key, value) -> System.out.println("Key: " + key + ", value: " + value));
     }
 }
