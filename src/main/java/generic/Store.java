c0a8a9d34ec3ee350f9b466d1685805cfb62e2d6
Gerik228 <dimaakulichd6618@gmail.com>
2026-02-24 14:08:13 +1000

    generic

---
+++ src/main/java/generic/Store.java
@@ -0,0 +1,9 @@
+package generic;
+
+public interface Store<T extends Base> {
+
+    void add(T model);
+    boolean replace(String id, T model);
+    void delete(String id);
+    T findById(String id);
+}
