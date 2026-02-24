036883815c21aa5f1eff0e1bcbceba6821e8a962
Gerik228 <dimaakulichd6618@gmail.com>
2026-02-24 12:52:06 +1000

    Balancer

---
+++ src/main/java/iterator/Balancer.java
@@ -0,0 +1,13 @@
+package iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        /* тут должна быть реализация */
+
+
+    }
+}
