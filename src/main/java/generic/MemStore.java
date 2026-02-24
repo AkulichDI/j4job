589a8724f740e889c70f231ec21f9a32f263cab6
Gerik228 <dimaakulichd6618@gmail.com>
2026-02-24 15:47:33 +1000

    UserStoreTest

--- src/main/java/generic/MemStore.java
+++ src/main/java/generic/MemStore.java
@@ -8,21 +8,32 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public void add(T model) {
+        if ( !storage.containsKey(model.getId())) {
+            storage.put(model.getId(), model);
+        }
 
     }
     @Override
     public boolean replace(String id, T model) {
+
+        if (storage.containsKey(id)) {
+            storage.replace(id , model);
+            return true;
+        }
 
         return false;
 
     }
     @Override
     public void delete(String id) {
-
+        storage.remove(id);
     }
+
     @Override
     public T findById(String id) {
-
+        if (storage.containsKey(id)) {
+            return storage.get(id);
+        }
         return null;
 
     }
