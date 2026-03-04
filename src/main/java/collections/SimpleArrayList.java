2a999d60a014f78f1ca08fa377af066b44d1c9c8
Akulich_DI <dimaakulichd6618@gmail.com>
2026-03-04 17:57:57 +1000

    Динамический список

---
+++ src/main/java/collections/SimpleArrayList.java
@@ -0,0 +1,58 @@
+package collections;
+
+import java.util.Iterator;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    private void resize(){
+
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public Object next() {
+                return null;
+            }
+        };
+    }
+}
