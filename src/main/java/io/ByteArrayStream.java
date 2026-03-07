1cce7139f73b1ec6e9fe3105caeb43b0d47f5677
Akulich_DI <dimaakulichd6618@gmail.com>
2026-03-08 09:54:48 +1000

    4. Потоки байтовых массивов (ByteArray Streams)

--- src/main/java/io/ByteArrayStream.java
+++ src/main/java/io/ByteArrayStream.java
@@ -1,15 +1,20 @@
 package io;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class ByteArrayStream {
 
 
     public static void main(String[] args) {
 
+        System.out.println();
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] bytes2 = "Message".getBytes();
+        outStream.writeBytes(bytes2);
+        System.out.println(outStream);
 
-
-
+/*
         byte[] bytes = new byte[]{'J', 'a', 'v', 'a'};
         ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
 
@@ -19,5 +24,8 @@ public class ByteArrayStream {
             System.out.println((char) data);
         }
 
+
+ */
     }
 }
+
