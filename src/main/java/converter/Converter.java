b429e1c8520c82fa065d508341b57f87995c4eda
Gerik228 <dimaakulichd6618@gmail.com>
2026-03-12 09:13:17 +1000

    Повторение junit5

--- src/main/java/converter/Converter.java
+++ src/main/java/converter/Converter.java
@@ -9,7 +9,7 @@ public class Converter {
 
 
     public static float rubleToDollar(float value) {
-        return value / 110;
+        return value / 60;
 
     }
 
@@ -20,8 +20,11 @@ public class Converter {
         float output1 = Converter.rubleToDollar(input);
         System.out.println(output1);
         boolean passed = expected == output;
-        boolean passed1 = expected == output1;
+        boolean passed1 = expected == output1;/*
         System.out.println("140 rubles are 2. Test result : " + passed);
         System.out.println("110 rubles are 1. Test result : " + passed1);
+        */
+
+        System.out.printf("%.15f%n", 23.123f);
     }
 }
