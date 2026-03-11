ce43ea9903b36124a005000ece6c1f519193e0a7
Akulich_DI <dimaakulichd6618@gmail.com>
2026-03-11 21:31:42 +1000

    assert

---
+++ src/main/java/converter/Converter.java
@@ -0,0 +1,27 @@
+package converter;
+
+public class Converter {
+
+
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+
+
+    public static float rubleToDollar(float value) {
+        return value / 110;
+
+    }
+
+    public static void main(String[] args) {
+        float input = 110;
+        float expected = 1;
+        float output = Converter.rubleToEuro(input);
+        float output1 = Converter.rubleToDollar(input);
+        System.out.println(output1);
+        boolean passed = expected == output;
+        boolean passed1 = expected == output1;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        System.out.println("110 rubles are 1. Test result : " + passed1);
+    }
+}
