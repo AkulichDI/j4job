ba2d12d7f75e6558fa01c2a38e1b7c0ed7d56e63
Gerik228 <dimaakulichd6618@gmail.com>
2026-03-12 09:30:33 +1000

    Повторение junit5

---
+++ src/main/java/calculator/MathCalculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+
+import static math.MathFunction.*;
+
+
+public class MathCalculator {
+
+    public static double sumAndMultiply ( double first , double second ){
+        return sum(first,second)
+                + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10,20));
+    }
+
+}
